8e2b7929697b6c4f9b1b483cbc9cef47f126a43d
Cabral Williams <cabral.williams@gmail.com>
2021-11-03 13:46:46 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,16 @@ const Sequelize = require('sequelize');
 
 
 //Create database connection
-const sequelize = new Sequelize(process.env.DB_NAME,'root',process.env.MYSQL_PASSWORD, {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306
-});
+let sequelize;
+
+if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME,'root',process.env.MYSQL_PASSWORD, {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
